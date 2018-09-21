7f98c608d98e65402e7e9b28120b4f02383e0396
karnaph <naphyk5@students.rowan.edu>
2018-09-21 17:42:21 -0400

    F5529 Simple Blink

---
+++ Simple Blink/New folder/main.c
@@ -0,0 +1,20 @@
+#include <msp430.h> 
+
+
+/**
+ * main.c
+ */
+int main(void)
+{
+    WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
+
+    P1DIR |= BIT0; //Sets P1 as output
+
+    while (1)
+    {
+        P1OUT ^= BIT0; //toggles LED
+        __delay_cycles(1000000); //delay so that doesn't blink too fast
+    }
+
+    return 0;
+}
